a3dc47239b77a8ed4504483dd30058b76cab2366
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-09-18 09:05:05 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Quiz vui lập trình</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
